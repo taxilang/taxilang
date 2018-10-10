4e09660916a8e1c884a165040561657a62cf998b
Marty Pitt <martypitt@gmail.com>
2018-10-10 21:41:31 +0100

    Changed default folder to `src`

--- taxi-cli/src/main/java/lang/taxi/cli/Project.kt
+++ taxi-cli/src/main/java/lang/taxi/cli/Project.kt
@@ -4,7 +4,7 @@ import com.typesafe.config.Config
 import lang.taxi.plugins.Artifact
 
 data class TaxiProject(
-        val sourceFolder: String = "src/main/taxi",
+        val sourceFolder: String = "src",
         val outputFolder:String = "target",
         val plugins: Map<String, Config>) {
     val pluginArtifacts: Map<Artifact, Config> by lazy {
