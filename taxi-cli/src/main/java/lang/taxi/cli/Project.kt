d82650df7ac92c7f9b21bc6dbbd3cf7a609c6cef
Marty Pitt <martypitt@gmail.com>
2019-03-18 18:26:09 +0000

    Minor refactor for externalisation of build command, to allow calling from other projects

--- taxi-cli/src/main/java/lang/taxi/cli/Project.kt
+++ taxi-cli/src/main/java/lang/taxi/cli/Project.kt
@@ -5,8 +5,8 @@ import lang.taxi.plugins.Artifact
 
 data class TaxiProject(
         val sourceFolder: String = "src",
-        val outputFolder:String = "target",
-        val plugins: Map<String, Config>) {
+        val outputFolder: String = "target",
+        val plugins: Map<String, Config> = emptyMap()) {
     val pluginArtifacts: Map<Artifact, Config> by lazy {
         this.plugins.mapKeys { (key, _) -> Artifact.parse(key) }
     }
