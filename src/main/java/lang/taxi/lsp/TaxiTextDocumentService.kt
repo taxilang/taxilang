77ba03c280c7bef24ac77db3012e386caab6b584
Marty Pitt <martypitt@gmail.com>
2020-04-22 07:05:42 +0100

    feat(completion): list types during completion

--- src/main/java/lang/taxi/lsp/TaxiTextDocumentService.kt
+++ src/main/java/lang/taxi/lsp/TaxiTextDocumentService.kt
@@ -2,43 +2,27 @@ package lang.taxi.lsp
 
 import lang.taxi.CompilationException
 import lang.taxi.Compiler
+import lang.taxi.TaxiDocument
+import lang.taxi.lsp.completion.TypeProvider
 import org.eclipse.lsp4j.*
 import org.eclipse.lsp4j.jsonrpc.messages.Either
 import org.eclipse.lsp4j.services.LanguageClient
 import org.eclipse.lsp4j.services.LanguageClientAware
 import org.eclipse.lsp4j.services.TextDocumentService
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.atomic.AtomicReference
 
 
 class TaxiTextDocumentService : TextDocumentService, LanguageClientAware {
+    private val masterDocument: AtomicReference<TaxiDocument> = AtomicReference();
+    private val compiledDocuments: MutableMap<String, TaxiDocument> = mutableMapOf()
+    private val typeProvider = TypeProvider(masterDocument)
     private lateinit var client: LanguageClient
     override fun completion(position: CompletionParams?): CompletableFuture<Either<MutableList<CompletionItem>, CompletionList>> {
-        // Provide completion item.
+
         // Provide completion item.
         return CompletableFuture.supplyAsync<Either<MutableList<CompletionItem>, CompletionList>> {
-            val completionItems: MutableList<CompletionItem> = ArrayList()
-            try {
-                val p = position
-                // Sample Completion item for sayHello
-                val completionItem = CompletionItem()
-                // Define the text to be inserted in to the file if the completion item is selected.
-                completionItem.insertText = "sayHello() {\n    print(\"hello\")\n}"
-                // Set the label that shows when the completion drop down appears in the Editor.
-                completionItem.label = "sayHello()"
-                // Set the completion kind. This is a snippet.
-                // That means it replace character which trigger the completion and
-                // replace it with what defined in inserted text.
-                completionItem.kind = CompletionItemKind.Snippet
-                // This will set the details for the snippet code which will help user to
-                // understand what this completion item is.
-                completionItem.detail = "sayHello()\n this will say hello to the people"
-
-                // Add the sample completion item to the list.
-                completionItems.add(completionItem)
-            } catch (e: Exception) {
-                //TODO: Handle the exception.
-            }
-            Either.forLeft(completionItems)
+            Either.forLeft(typeProvider.getTypes().toMutableList())
         }
     }
 
@@ -60,10 +44,15 @@ class TaxiTextDocumentService : TextDocumentService, LanguageClientAware {
             TODO("Ranged changes not yet supported")
         }
         val content = change.text
-        val uri = params.textDocument.uri
+        val sourceName = params.textDocument.uri
+        val uri = sourceName
         try {
-            Compiler(content, params.textDocument.uri).compile()
-            client.publishDiagnostics(PublishDiagnosticsParams(params.textDocument.uri, emptyList(), params.textDocument.version))
+            val importSources = compiledDocuments.filterKeys { it != sourceName }
+                    .values.toList()
+            val compiled = Compiler(content, sourceName, importSources).compile()
+            compiledDocuments[sourceName] = compiled
+            masterDocument.set(compiled)
+            client.publishDiagnostics(PublishDiagnosticsParams(sourceName, emptyList(), params.textDocument.version))
         } catch (e: CompilationException) {
             val diagnostics = e.errors.map { error ->
                 // Note - for VSCode, we can use the same position for start and end, and it
@@ -80,7 +69,7 @@ class TaxiTextDocumentService : TextDocumentService, LanguageClientAware {
                 )
             }
             client.publishDiagnostics(PublishDiagnosticsParams(
-                    params.textDocument.uri,
+                    sourceName,
                     diagnostics,
                     params.textDocument.version
             ))
---
+++ src/main/java/lang/taxi/lsp/completion/TypeProvider.kt
@@ -0,0 +1,35 @@
+package lang.taxi.lsp.completion
+
+import lang.taxi.TaxiDocument
+import lang.taxi.types.Documented
+import lang.taxi.types.PrimitiveType
+import org.eclipse.lsp4j.CompletionItem
+import org.eclipse.lsp4j.CompletionItemKind
+import java.util.concurrent.atomic.AtomicReference
+
+class TypeProvider(val taxiDocument: AtomicReference<TaxiDocument>) {
+    val primitives = PrimitiveType.values()
+            .map { type ->
+                CompletionItem(type.declaration).apply {
+                    kind = CompletionItemKind.Class
+                    insertText = type.declaration
+                    detail = type.typeDoc
+                }
+            }
+
+    fun getTypes(): List<CompletionItem> {
+        val types = taxiDocument.get()?.types ?: emptySet()
+        val completionItems = types.map { type ->
+            val name = type.toQualifiedName()
+            val doc = if (type is Documented) {
+                type.typeDoc
+            } else null
+            CompletionItem(name.typeName).apply {
+                kind = CompletionItemKind.Class
+                insertText = type.toQualifiedName().typeName
+                detail = listOfNotNull(type.qualifiedName, doc).joinToString("\n")
+            }
+        }
+        return completionItems + primitives
+    }
+}
