2096e875148c3e915aa75dbe372de3250b11e02e
Marty Pitt <martypitt@gmail.com>
2020-05-27 15:37:58 +0100

    fix: types will resolve unambgiously if the named type exsits in the same namespace

--- compiler/src/test/java/lang/taxi/TypeSystemTest.kt
+++ compiler/src/test/java/lang/taxi/TypeSystemTest.kt
@@ -2,6 +2,7 @@ package lang.taxi
 
 import com.winterbe.expekt.should
 import lang.taxi.types.ObjectType
+import lang.taxi.types.QualifiedName
 import org.assertj.core.api.Assertions
 import org.junit.Assert.*
 import org.junit.Test
@@ -24,9 +25,20 @@ class TypeSystemTest {
    }
 
    @Test
-   fun when_aTypeIsImportedAndAlsoDeclared_then_qualificationThrowsAmbiguousError() {
+   fun when_aTypeResolvesToMultipleTypes_then_qualificationThrowsAmbiguousError() {
       val typeSystem = TypeSystem(listOf(ObjectType.undefined("foo.Customer"), ObjectType.undefined("bar.Customer")))
-      Assertions.assertThatThrownBy { typeSystem.qualify("bar", "Customer") }.hasMessageContaining("Name reference Customer is ambiguous, and could refer to any of the available types foo.Customer, bar.Customer")
+      Assertions.assertThatThrownBy { typeSystem.qualify("xxx", "Customer") }.hasMessageContaining("Name reference Customer is ambiguous, and could refer to any of the available types foo.Customer, bar.Customer")
+   }
+
+   @Test
+   fun `when qualifying types if there is a match in the current namespace that hasn't been imported, then it is resolved`() {
+      val typeSystem = TypeSystem(listOf(ObjectType.undefined("foo.Customer"), ObjectType.undefined("bar.Customer")))
+      typeSystem.qualify("foo","Customer").should.equal("foo.Customer")
+   }
+   @Test
+   fun `when qualifying types if there is an explict import AND a match in the current namespace, then the import wins`() {
+      val typeSystem = TypeSystem(listOf(ObjectType.undefined("foo.Customer"), ObjectType.undefined("bar.Customer")))
+      typeSystem.qualify("foo","Customer", listOf(QualifiedName.from("bar.Customer"))).should.equal("bar.Customer")
    }
 
 }
