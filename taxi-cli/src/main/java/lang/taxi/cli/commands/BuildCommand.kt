7c5c2fa6b6309592cbf894422ea852906b26fbee
devrim.bostanci <devrim.bostanci@sigun.com.tr>
2020-06-03 13:29:14 +0300

    LENS-124 Compilations using CLI, kotlin code generation and maven build is done. Tests are coming soon.

--- taxi-cli/src/main/java/lang/taxi/cli/commands/BuildCommand.kt
+++ taxi-cli/src/main/java/lang/taxi/cli/commands/BuildCommand.kt
@@ -1,22 +1,31 @@
 package lang.taxi.cli.commands
 
+import com.sun.org.apache.xalan.internal.xsltc.cmdline.Compile
 import lang.taxi.Compiler
 import lang.taxi.TaxiDocument
 import lang.taxi.cli.config.TaxiConfig
 import lang.taxi.cli.config.TaxiEnvironment
-import lang.taxi.plugins.Plugin
 import lang.taxi.cli.plugins.PluginRegistry
 import lang.taxi.cli.utils.log
 import lang.taxi.generators.ModelGenerator
 import lang.taxi.generators.Processor
 import lang.taxi.generators.WritableSource
-import lang.taxi.generators.kotlin.BaseAnnotationInjector
-import lang.taxi.plugins.ComponentProviderPlugin
+import lang.taxi.packages.TaxiSourcesLoader
+import lang.taxi.plugins.Plugin
 import org.antlr.v4.runtime.CharStreams
 import org.apache.commons.io.FileUtils
+import org.apache.maven.model.Dependency
+import org.apache.maven.model.Model
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer
 import org.springframework.stereotype.Component
+import java.io.File
+import java.io.FileWriter
+import java.io.Writer
 import java.nio.charset.Charset
 import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.*
+
 
 @Component
 class BuildCommand(val config: TaxiConfig, val pluginManager: PluginRegistry) : ShellCommand {
@@ -37,6 +46,7 @@ class BuildCommand(val config: TaxiConfig, val pluginManager: PluginRegistry) : 
                 }
 
         writeSources(sourcesToOutput, environment.outputPath)
+       generatePom(environment.outputPath)
         log().info("Wrote ${sourcesToOutput.size} files to ${environment.outputPath}")
     }
 
@@ -54,17 +64,31 @@ class BuildCommand(val config: TaxiConfig, val pluginManager: PluginRegistry) : 
     }
 
     private fun loadSources(path: Path): TaxiDocument? {
-        val files = path.toFile().walkTopDown()
-                .filter { it.isFile }
-                .filter { it.extension == "taxi" }
-                .map { file ->
-                    CharStreams.fromPath(file.toPath())
-                }.toList()
-        if (files.isEmpty()) {
-            log().warn("No sources were found to compile at $path - exiting")
-            return null
-        }
-        return Compiler(files.toList()).compile()
+       val taxiProject = TaxiSourcesLoader.loadPackage(path)
+
+       return Compiler(taxiProject).compile()
     }
+
+   private fun generatePom(path: Path) {
+      val model = Model()
+      val writer: Writer = FileWriter("${path}/pom.xml")
+      val dependencyList: MutableList<Dependency> = ArrayList<Dependency>()
+
+      // TODO tidy up
+      model.modelVersion = "4.0.0"
+      model.groupId = "lang.taxi"
+      model.artifactId = "parent"
+      model.version = "0.5.0-SNAPSHOT"
+
+      val dep = Dependency()
+      dep.groupId = "lang.taxi"
+      dep.artifactId = "taxi-annotations"
+      dep.version = "0.5.0-SNAPSHOT"
+      dependencyList.add(dep)
+
+      model.dependencies = dependencyList
+      MavenXpp3Writer().write(writer, model)
+      writer.close()
+   }
 
 }
--- taxi-cli/src/main/java/lang/taxi/cli/config/TaxiEnvironment.kt
+++ taxi-cli/src/main/java/lang/taxi/cli/config/TaxiEnvironment.kt
@@ -1,16 +1,23 @@
 package lang.taxi.cli.config
 
 import lang.taxi.cli.CliOptions
+import lang.taxi.cli.TaxiCli
 import org.springframework.stereotype.Component
 import java.nio.file.Path
 import java.nio.file.Paths
 
 @Component
 data class TaxiEnvironment(
-        val config: TaxiConfig,
-        val cliOptions: CliOptions
-) {
-    val sourcePath: Path = Paths.get(cliOptions.projectHome, config.project.sourceFolder)
+        val config: TaxiConfig//,
 
-    val outputPath:Path = Paths.get(cliOptions.projectHome, config.project.outputFolder)
+   // TODO using bootOptions as a static object until cliOptions is arranged.
+//        val cliOptions: CliOptions
+) {
+   // TODO using bootOptions as a static object until cliOptions is arranged.
+//    val sourcePath: Path = Paths.get(cliOptions.projectHome, config.project.sourceFolder)
+    val sourcePath: Path = Paths.get(TaxiCli.bootOptions.projectHome, config.project.sourceFolder)
+
+   // TODO using bootOptions as a static object until cliOptions is arranged.
+//    val outputPath:Path = Paths.get(cliOptions.projectHome, config.project.outputFolder)
+    val outputPath:Path = Paths.get(TaxiCli.bootOptions.projectHome, config.project.outputFolder)
 }
