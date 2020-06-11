d59261b65cdba4ad47d56b188e0a9edc4ca27f37
devrim.bostanci <devrim.bostanci@sigun.com.tr>
2020-06-11 12:41:06 +0300

    Possible bug in TaxiProjectLoader

--- package-importer/src/test/java/lang/taxi/packages/TaxiProjectLoaderTest.kt
+++ package-importer/src/test/java/lang/taxi/packages/TaxiProjectLoaderTest.kt
@@ -2,7 +2,14 @@ package lang.taxi.packages
 
 import com.google.common.io.Resources
 import com.winterbe.expekt.should
+import org.apache.commons.io.FileUtils
+import org.apache.commons.lang3.SystemUtils
+import org.junit.After
+import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
+import java.io.File
+import java.net.URI
 import java.nio.file.Paths
 
 class TaxiProjectLoaderTest {
@@ -12,5 +19,20 @@ class TaxiProjectLoaderTest {
       val config = TaxiProjectLoader().withConfigFileAt(Paths.get(resource)).load()
       config.identifier.id.should.equal("taxi/Demo/0.2.0")
       config.dependencies["taxi/Another"].should.equal("0.3.0")
+   }
+
+   @Ignore
+   @Test
+   fun taxiProjectLoaderBug() {
+      File(Resources.getResource("testRepo/taxi/lang.taxi.Dummy/0.2.0/anotherConfig/taxi.conf").toURI())
+         .copyTo(File("${SystemUtils.getUserHome()}/.taxi/taxi.conf"), true)
+      val anotherConfig = File("${SystemUtils.getUserHome()}/.taxi/taxi.conf")
+
+      val resource = Resources.getResource("testRepo/taxi/lang.taxi.Dummy/0.2.0/taxi.conf").toURI()
+      val config = TaxiProjectLoader().withConfigFileAt(Paths.get(resource)).load()
+      config.identifier.id.should.equal("taxi/Demo/0.2.0")
+      config.dependencies["taxi/Another"].should.equal("0.3.0")
+
+      anotherConfig.delete()
    }
 }
