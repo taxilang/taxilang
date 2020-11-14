6b802fc58c9f9a868d83be23e5817101aefc7db5
Marty Pitt <martypitt@gmail.com>
2020-11-14 07:39:36 +0000

    bumped version

--- core-types/src/main/java/lang/taxi/services/Service.kt
+++ core-types/src/main/java/lang/taxi/services/Service.kt
@@ -10,48 +10,70 @@ import lang.taxi.types.Compiled
 import lang.taxi.types.Documented
 import lang.taxi.types.NameTypePair
 import lang.taxi.types.Named
+import lang.taxi.types.TaxiStatementGenerator
 import lang.taxi.types.Type
 import lang.taxi.types.toSet
 
-data class Parameter(override val annotations: List<Annotation>, override val type: Type, override val name: String?, override val constraints: List<Constraint>, val isVarArg:Boolean = false) : Annotatable, ConstraintTarget, NameTypePair {
+data class Parameter(override val annotations: List<Annotation>, override val type: Type, override val name: String?, override val constraints: List<Constraint>, val isVarArg: Boolean = false) : Annotatable, ConstraintTarget, NameTypePair {
    override val description: String = "param $name"
 }
 
 interface ServiceMember : Annotatable, Compiled, Documented {
-   val name:String
+   val name: String
 }
+
 data class QueryOperation(
-   override val name:String,
+   override val name: String,
    override val annotations: List<Annotation>,
-   val grammar:String,
+   val grammar: String,
    val returnType: Type,
    override val compilationUnits: List<CompilationUnit>,
-   val capabilities:List<QueryOperationCapability>,
+   val capabilities: List<QueryOperationCapability>,
    override val typeDoc: String? = null) : ServiceMember, Annotatable, Compiled, Documented {
    private val equality = Equality(this, QueryOperation::name, QueryOperation::annotations, QueryOperation::returnType)
 
    override fun equals(other: Any?) = equality.isEqualTo(other)
    override fun hashCode(): Int = equality.hash()
 }
-interface QueryOperationCapability
-data class FilterCapability(val supportedOperations:List<FilterOperation>):QueryOperationCapability {
-   enum class FilterOperation {
-      EQUAL,
-      IN,
-      LIKE,
-      GREATER_THAN,
-      GREATER_THAN_EQUALS,
-      LESS_THAN,
-      LESS_THAN_EQUALS
+
+interface QueryOperationCapability : TaxiStatementGenerator {
+   companion object {
+      val ALL: List<QueryOperationCapability> = SimpleQueryCapability.values().toList() +
+         listOf(
+            FilterCapability(FilterCapability.FilterOperation.values().toList())
+         )
    }
 }
+
+data class FilterCapability(val supportedOperations: List<FilterOperation>) : QueryOperationCapability {
+   enum class FilterOperation(val symbol:String) {
+      EQUAL("="),
+      IN("in"),
+      LIKE("like"),
+      GREATER_THAN(">"),
+      GREATER_THAN_EQUALS(">="),
+      LESS_THAN("<"),
+      LESS_THAN_EQUALS("<=")
+   }
+
+   override fun asTaxi(): String {
+      return "filter(${this.supportedOperations.joinToString(",") {it.symbol}})"
+   }
+}
+
 enum class SimpleQueryCapability : QueryOperationCapability {
    SUM,
    COUNT,
    AVG,
    MIN,
-   MAX
+   MAX;
+
+   override fun asTaxi(): String {
+      return this.name.toLowerCase()
+   }
+
 }
+
 data class Operation(override val name: String,
                      val scope: String? = null,
                      override val annotations: List<Annotation>,
@@ -77,8 +99,8 @@ data class Service(override val qualifiedName: String,
    override fun equals(other: Any?) = equality.isEqualTo(other)
    override fun hashCode(): Int = equality.hash()
 
-   val operations:List<Operation> = this.members.filterIsInstance<Operation>()
-   val queryOperations:List<QueryOperation> = this.members.filterIsInstance<QueryOperation>()
+   val operations: List<Operation> = this.members.filterIsInstance<Operation>()
+   val queryOperations: List<QueryOperation> = this.members.filterIsInstance<QueryOperation>()
 
    fun operation(name: String): Operation {
       return this.operations.first { it.name == name }
