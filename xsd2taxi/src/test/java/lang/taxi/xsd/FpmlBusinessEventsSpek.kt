01e358eae79e429253a0b5d75267cad02af5cfdd
Marty Pitt <martypitt@gmail.com>
2021-01-29 15:30:00 +0000

    added test around fpml spec

--- xsd2taxi/src/test/java/lang/taxi/xsd/FpmlBusinessEventsSpek.kt
+++ xsd2taxi/src/test/java/lang/taxi/xsd/FpmlBusinessEventsSpek.kt
@@ -10,18 +10,7 @@ import org.spekframework.spek2.style.specification.describe
 object FpmlBusinessEventsSpek : Spek({
    describe("parsing fpml") {
       it("should parse a business events file") {
-         val (generated,errors) = compileXsdResource("samples/fpml/confirmation-5-10_xml/confirmation/fpml-business-events-5-10.xsd")
-
-         errors.should.be.empty
-         val expected = Resources.getResource("samples/fpml/confirmation-5-10_xml/confirmation/expected-taxi/fpml.taxi")
-            .readText()
-
-         val generatedDoc = Compiler.forStrings(generated.taxi).compile()
-         val expectedDoc = Compiler.forStrings(xsdTaxiSources(expected)).compile()
-         TestHelpers.expectToCompileTheSame(generated.taxi, xsdTaxiSources(expected))
-      }
-      it("should parse the swaps file") {
-         val (generated,errors) = compileXsdResource("samples/fpml/confirmation-5-10_xml/confirmation/fpml-ird-5-10.xsd")
+         val (generated, errors) = compileXsdResource("samples/fpml/confirmation-5-10_xml/confirmation/fpml-business-events-5-10.xsd")
 
          errors.should.be.empty
          val expected = Resources.getResource("samples/fpml/confirmation-5-10_xml/confirmation/expected-taxi/fpml.taxi")
