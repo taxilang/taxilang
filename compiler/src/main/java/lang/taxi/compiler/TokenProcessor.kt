5c309f4bcb772406f79c91e00c424c36fede9945
Serhat Tuncay <serhat.tuncay@vyne.co>
2021-03-21 20:59:42 +0300

    View Definition

--- compiler/src/main/java/lang/taxi/compiler/TokenProcessor.kt
+++ compiler/src/main/java/lang/taxi/compiler/TokenProcessor.kt
@@ -40,6 +40,7 @@ import lang.taxi.services.QueryOperationCapability
 import lang.taxi.services.SimpleQueryCapability
 import lang.taxi.types.*
 import lang.taxi.types.Annotation
+import lang.taxi.types.View.Companion.JoinAnnotationName
 import lang.taxi.utils.errorOrNull
 import lang.taxi.utils.flattenErrors
 import lang.taxi.utils.invertEitherList
@@ -56,7 +57,7 @@ internal class TokenProcessor(
    importSources: List<TaxiDocument> = emptyList(),
    collectImports: Boolean = true,
    val typeChecker: TypeChecker,
-   private val linter:Linter
+   private val linter: Linter
 ) {
 
    companion object {
@@ -66,7 +67,7 @@ internal class TokenProcessor(
 
    }
 
-   constructor(tokens: Tokens, collectImports: Boolean, typeChecker: TypeChecker, linter:Linter = Linter.empty()) : this(
+   constructor(tokens: Tokens, collectImports: Boolean, typeChecker: TypeChecker, linter: Linter = Linter.empty()) : this(
       tokens,
       emptyList(),
       collectImports,
@@ -81,6 +82,7 @@ internal class TokenProcessor(
    private val policies = mutableListOf<Policy>()
    private val functions = mutableListOf<Function>()
    private val annotations = mutableListOf<Annotation>()
+   private val views = mutableListOf<View>()
    private val constraintValidator = ConstraintValidator()
 
    private val errors = mutableListOf<CompilationError>()
@@ -109,7 +111,7 @@ internal class TokenProcessor(
       compile()
       // TODO: Unsure if including the imported types here is a good iddea or not.
       val types = typeSystem.typeList(includeImportedTypes = true).toSet()
-      return errors to TaxiDocument(types, services.toSet(), policies.toSet(), functions.toSet())
+      return errors to TaxiDocument(types, services.toSet(), policies.toSet(), functions.toSet(), annotations.toSet(), views.toSet())
    }
 
    fun buildQueries(): Pair<List<CompilationError>, List<TaxiQlQuery>> {
@@ -220,7 +222,229 @@ internal class TokenProcessor(
 
       // Queries
       compileQueries()
+      //
+      compileViews()
    }
+
+   private fun compileViews() {
+      this.tokens.unparsedViews.map { entry ->
+         compileView(entry.key, entry.value.first, entry.value.second)
+      }.invertEitherList()
+         .flattenErrors()
+         .collectErrors(errors)
+         .map { this.views.addAll(it) }
+   }
+
+   private fun compileView(
+      viewName: String,
+      namespace: Namespace,
+      viewCtx: TaxiParser.ViewDeclarationContext): Either<List<CompilationError>, View> {
+      val viewDocumentation = parseTypeDoc(viewCtx.typeDoc())
+      val annotations = collateAnnotations(viewCtx.annotation())
+      val modifiers = parseModifiers(viewCtx.typeModifier())
+      val inherits = parseTypeInheritance(namespace, viewCtx.listOfInheritedTypes())
+      val viewBodies = compileViewBody(namespace, viewCtx.findBody())
+      return viewBodies.map { viewBodies ->
+         val viewDefinition = ViewDefinition(
+            inheritsFrom = inherits,
+            annotations = annotations.toSet(),
+            modifiers = modifiers,
+            typeDoc = viewDocumentation,
+            viewBodyDefinitions = viewBodies,
+            compilationUnit = viewCtx.toCompilationUnit())
+         View(viewName, viewDefinition)
+      }
+   }
+
+   private fun compileViewBody(namespace: Namespace, bodyContexts: List<TaxiParser.FindBodyContext>):
+      Either<List<CompilationError>, List<ViewBodyDefinition>> {
+      val compilationErrors = mutableListOf<CompilationError>()
+      val viewBodyDefinitions = mutableListOf<ViewBodyDefinition>()
+      bodyContexts.forEach { bodyCtx ->
+         resolveViewBody(namespace, bodyCtx)
+            .map { viewBodyDefinitions.add(it) }
+            .mapLeft { errors -> compilationErrors.addAll(errors) }
+      }
+
+      return if (compilationErrors.isNotEmpty()) {
+         compilationErrors.left()
+      } else {
+         viewBodyDefinitions.right()
+      }
+   }
+
+   private fun validateMultipleBodyDefinitions(bodyDefinitions: List<ViewBodyDefinition>) {
+      if (bodyDefinitions.size == 1) {
+         bodyDefinitions.right()
+      }
+
+   }
+
+   private fun resolveViewBody(namespace: Namespace, bodyCtx: TaxiParser.FindBodyContext):
+      Either<List<CompilationError>, ViewBodyDefinition> {
+      val joinTypes = bodyCtx.findBodyQuery().joinTo().typeType()
+      return listTypeTypeOrError(joinTypes.first()).flatMap { bodyTypeType ->
+         typeOrError(namespace, bodyTypeType).flatMap { bodyType ->
+            if (joinTypes.size > 1) {
+               val joinTypeTypeCtx = joinTypes[1]
+               listTypeTypeOrError(joinTypeTypeCtx).flatMap { joinType ->
+                  typeOrError(namespace, joinTypeTypeCtx).flatMap { joinType ->
+                     validateJoinBasedViewBodyDefinition(bodyCtx, ViewBodyDefinition(bodyType, joinType))
+                        .flatMap { validViewDefinition ->
+                           if (bodyCtx.typeBody() != null) {
+                              parseViewBodyDefinition(namespace, bodyCtx).flatMap { viewBodyTypeDefinition ->
+                                 validViewDefinition.copy(viewBodyTypeDefinition = viewBodyTypeDefinition).right()
+                              }.mapLeft { it }
+                           } else {
+                              validViewDefinition.right()
+                           }
+                        }
+                        .mapLeft { it }
+                  }.mapLeft { it }
+               }.mapLeft { it }
+            } else {
+               if (bodyCtx.typeBody() != null) {
+                  parseViewBodyDefinition(namespace, bodyCtx).flatMap { viewBodyTypeDefinition ->
+                     ViewBodyDefinition(bodyType).copy(viewBodyTypeDefinition = viewBodyTypeDefinition).right()
+                  }.mapLeft { it }
+               } else {
+                  ViewBodyDefinition(bodyType).right()
+               }
+            }
+         }
+      }.mapLeft { it }
+   }
+
+   private fun parseViewBodyDefinition(namespace: Namespace, bodyCtx: TaxiParser.FindBodyContext): Either<List<CompilationError>, ViewBodyTypeDefinition> {
+      val typeBody = bodyCtx.typeBody()
+      if (!typeBody.calculatedMemberDeclaration().isNullOrEmpty()) {
+         return listOf(CompilationError(typeBody.start, "Calculated member declaration, ${typeBody.calculatedMemberDeclaration().first().text}, is not allowed!")).left()
+      }
+      val definitionErrors = typeBody.typeMemberDeclaration().map { typeMemberDeclarationContext ->
+         val fieldDeclarationContext = typeMemberDeclarationContext.fieldDeclaration()
+         val fieldName = fieldDeclarationContext.Identifier().text
+         val simplifiedDeclaration = fieldDeclarationContext.simpleFieldDeclaration()
+                 ?: return listOf(CompilationError(typeBody.start, "Invalid find type body field declaration expected TypeName.TypeName")).left()
+
+         val classOrInterfaceTypeCtx = simplifiedDeclaration.typeType().classOrInterfaceType()
+                 ?: return listOf(CompilationError(typeBody.start, "Invalid find type body field declaration expected TypeName.TypeName")).left()
+
+         val accessorCtx = simplifiedDeclaration.accessor()
+         val sourceTypeName = classOrInterfaceTypeCtx.Identifier().first().text
+         resolveUserType(namespace, sourceTypeName, bodyCtx, SymbolKind.TYPE_OR_MODEL)
+            .flatMap { sourceType ->
+               val sourceFieldTypeName = classOrInterfaceTypeCtx.Identifier().last().text
+               resolveUserType(namespace, sourceFieldTypeName, bodyCtx, SymbolKind.TYPE_OR_MODEL).flatMap { fieldSourceType ->
+                  validateViewBodyFieldType(sourceType, fieldSourceType, bodyCtx).flatMap { fieldType ->
+                     validateViewFindFieldAccessor(accessorCtx).flatMap { accessor ->
+                        ViewBodyFieldDefinition(sourceType = sourceType, fieldType = fieldType, fieldName = fieldName, accessor = accessor).right()
+                     }.mapLeft { it }
+                  }.mapLeft { it }
+               }.mapLeft { it }
+            }.mapLeft { it }
+      }
+
+
+      val definitions = mutableListOf<ViewBodyFieldDefinition>()
+      definitionErrors.forEach { definitionOrError ->
+         when (definitionOrError) {
+            is Either.Left -> return definitionOrError
+            is Either.Right -> definitions.add(definitionOrError.b)
+         }
+      }
+      return ViewBodyTypeDefinition(definitions.toList()).right()
+   }
+
+   private fun validateViewFindFieldAccessor(accessorCtx: TaxiParser.AccessorContext?): Either<List<CompilationError>, ConditionalAccessor?> {
+      if (accessorCtx == null) {
+         return Either.right(null)
+      }
+
+      if (accessorCtx.objectAccessor() != null) {
+         return listOf(CompilationError(accessorCtx.start, "only 'when' based definitions are allowed")).left()
+      }
+
+      val scalarAccessorCtx = accessorCtx.scalarAccessor()
+      val scalarAccessorExpressionCtx = scalarAccessorCtx.scalarAccessorExpression()
+      val conditonalTypeCtx = scalarAccessorExpressionCtx.conditionalTypeConditionDeclaration()
+              ?: return listOf(CompilationError(accessorCtx.start, "only 'when' based definitions are allowed")).left()
+      val fieldExpression = conditonalTypeCtx.fieldExpression()
+      if (fieldExpression != null) {
+         return listOf(CompilationError(accessorCtx.start, "when(this.xxx) not allowed, please use when { case1 -> value1...}")).left()
+      }
+      val whenExpression = conditonalTypeCtx.conditionalTypeWhenDeclaration()
+
+      return Either.right(null)
+   }
+
+   private fun validateViewBodyFieldType(sourceType: Type, fieldType: Type, bodyCtx: TaxiParser.FindBodyContext): Either<List<CompilationError>, Type> {
+      if (sourceType !is ObjectType) {
+         return listOf(CompilationError(bodyCtx.start, "${sourceType.qualifiedName} should be an object type")).left()
+      }
+      if (sourceType == fieldType) {
+         return fieldType.right()
+      }
+
+      val matches = sourceType.fields.filter { field ->
+          if (!field.type.format.isNullOrEmpty()) {
+            fieldType.isAssignableTo(field.type)
+         } else {
+            field.type == fieldType
+         }
+
+      }
+
+      return when {
+         matches.size != 1 -> listOf(CompilationError(bodyCtx.start, "${sourceType.qualifiedName} does not have 1 field with type ${fieldType.qualifiedName}")).left()
+         else -> matches.first().type.right()
+      }
+   }
+
+   private fun listTypeTypeOrError(typeTypeCtx: TaxiParser.TypeTypeContext): Either<List<CompilationError>, TaxiParser.TypeTypeContext> {
+      return if (typeTypeCtx.listType() == null) {
+         listOf(CompilationError(typeTypeCtx.start, "Currently, only list types are supported in view definitions. Replace ${typeTypeCtx.text} with ${typeTypeCtx.text}[]")).left()
+      } else {
+         typeTypeCtx.right()
+      }
+   }
+
+   private fun validateJoinBasedViewBodyDefinition(bodyCtx: TaxiParser.FindBodyContext, viewBodyDefinition: ViewBodyDefinition):
+      Either<List<CompilationError>, ViewBodyDefinition> {
+      val bodyType = viewBodyDefinition.bodyType
+      val joinType = viewBodyDefinition.joinType!!
+
+      if (bodyType !is ObjectType) {
+         return listOf(CompilationError(bodyCtx.start, "${bodyType.qualifiedName} is not an Object Type")).left()
+      }
+
+      if (joinType !is ObjectType) {
+         return listOf(CompilationError(bodyCtx.start, "${joinType.qualifiedName} is not an Object Type")).left()
+      }
+
+      val bodyHasAnIdField = bodyType.fields.filter { it.annotations.firstOrNull { annotation -> annotation.qualifiedName == JoinAnnotationName } != null }
+      val joinHasAnIdField = joinType.fields.filter { it.annotations.firstOrNull { annotation -> annotation.qualifiedName == JoinAnnotationName } != null }
+
+      if (bodyHasAnIdField.size == 1 &&
+         joinHasAnIdField.size == 1 &&
+         bodyHasAnIdField[0].type.basePrimitive != null &&
+         joinHasAnIdField[0].type.basePrimitive != null &&
+         bodyHasAnIdField[0].type.basePrimitive!!.isAssignableTo(joinHasAnIdField[0].type.basePrimitive!!)) {
+         return viewBodyDefinition.right()
+      }
+
+      val bodyFieldsQualifiedNames = bodyType.fields.map { it.type.qualifiedName }
+      val joinFieldsQualifiedNames = joinType.fields.map { it.type.qualifiedName }
+
+      val joinFieldCounts = bodyFieldsQualifiedNames
+         .count { bodyFieldTypeQualifiedName -> joinFieldsQualifiedNames.contains(bodyFieldTypeQualifiedName) }
+
+      return when (joinFieldCounts) {
+         1 -> viewBodyDefinition.right()
+         else -> listOf(CompilationError(bodyCtx.start, "${bodyType.qualifiedName} and ${joinType.qualifiedName} can't be joined")).left()
+      }
+
+   }
+
 
    private fun compileQueries() {
       this.tokens.anonymousQueries.forEach { (qualifiedName, anonymousQueryContex) ->
@@ -238,8 +462,8 @@ internal class TokenProcessor(
             val parameterName = queryParam.Identifier().text
             val queryParameter: Either<List<CompilationError>, Pair<String, QualifiedName>> =
                typeOrError(namedQueryContext.findNamespace(), queryParam.typeType()).map { parameterType ->
-               parameterName to parameterType.toQualifiedName()
-            }
+                  parameterName to parameterType.toQualifiedName()
+               }
             queryParameter
          }?.invertEitherList() ?: Either.right(emptyList())
          parametersOrErrors
@@ -250,8 +474,8 @@ internal class TokenProcessor(
                   .mapLeft { compilationErrors -> errors.addAll(compilationErrors) }
                   .map { taxiQlQuery -> queries.add(taxiQlQuery) }
             }
-         }
       }
+   }
 
    private fun applySynonymsToEnums() {
       // Now we have a full picture of all the enums, we can
@@ -782,14 +1006,14 @@ internal class TokenProcessor(
       return this.typeSystem.register(
          ObjectType(
             typeName, ObjectTypeDefinition(
-               fields = fields.toSet(),
-               annotations = annotations.toSet(),
-               modifiers = modifiers,
-               inheritsFrom = inherits,
-               format = null,
-               typeDoc = typeDoc,
-               compilationUnit = ctx.toCompilationUnit()
-            )
+            fields = fields.toSet(),
+            annotations = annotations.toSet(),
+            modifiers = modifiers,
+            inheritsFrom = inherits,
+            format = null,
+            typeDoc = typeDoc,
+            compilationUnit = ctx.toCompilationUnit()
+         )
          )
       ).right()
    }
@@ -1369,14 +1593,14 @@ internal class TokenProcessor(
             val isLenient = ctx.lenientKeyword() != null
             val enumType = EnumType(
                typeName, EnumDefinition(
-                  enumValues,
-                  annotations,
-                  ctx.toCompilationUnit(),
-                  inheritsFrom = if (inherits != null) setOf(inherits) else emptySet(),
-                  typeDoc = parseTypeDoc(ctx.typeDoc()),
-                  basePrimitive = basePrimitive,
-                  isLenient = isLenient
-               )
+               enumValues,
+               annotations,
+               ctx.toCompilationUnit(),
+               inheritsFrom = if (inherits != null) setOf(inherits) else emptySet(),
+               typeDoc = parseTypeDoc(ctx.typeDoc()),
+               basePrimitive = basePrimitive,
+               isLenient = isLenient
+            )
             )
             typeSystem.register(enumType)
             enumType
---
+++ compiler/src/test/java/lang/taxi/ViewSpec.kt
@@ -0,0 +1,279 @@
+package lang.taxi
+
+import com.winterbe.expekt.should
+import lang.taxi.types.ViewBodyFieldDefinition
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+class ViewSpec: Spek( {
+   describe("view syntax") {
+      it("simple view definition") {
+         val src = """
+         model Person {
+            firstName : FirstName as String
+            lastName : LastName as String
+         }
+
+          [[
+           Sample View
+          ]]
+         view PersonView with query {
+            find { Person[] }
+         }
+           """.trimIndent()
+         val taxiDocument = Compiler(src).compile()
+         taxiDocument.model("Person").should.not.be.`null`
+         val personView = taxiDocument.view("PersonView")
+         personView.should.not.be.`null`
+         personView?.typeDoc.should.equal("Sample View")
+      }
+
+      it ("view with a namespace") {
+         val src = """
+            namespace notional
+            {
+               model Person {
+                  firstName : FirstName as String
+                  lastName : LastName as String
+               }
+           }
+           """.trimIndent()
+
+         val viewSrc = """
+            namespace notional.views {
+                [[
+                 Sample View
+                ]]
+               view PersonView with query {
+                  find { notional.Person[] }
+               }
+            }
+         """.trimIndent()
+         val taxiDocument = Compiler.forStrings(listOf(src, viewSrc)).compile()
+         taxiDocument.model("notional.Person").should.not.be.`null`
+         val personView = taxiDocument.view("notional.views.PersonView")
+         personView.should.not.be.`null`
+         personView?.typeDoc.should.equal("Sample View")
+      }
+
+      it("A view with multiple view statements") {
+         val src = """
+         type OrderId inherits String
+         model OrderSent {
+            sentOrderId : OrderId
+         }
+
+         model OrderFill {
+           fillOrderId: OrderId
+         }
+
+          [[
+           Sample View
+          ]]
+         view OrderView with query {
+            find { OrderSent[] },
+            find { OrderSent[] (joinTo OrderFill[]) }
+         }
+           """.trimIndent()
+         val taxiDocument = Compiler(src).compile()
+         taxiDocument.model("OrderFill").should.not.be.`null`
+         taxiDocument.model("OrderSent").should.not.be.`null`
+         val orderView = taxiDocument.view("OrderView")
+         orderView.should.not.be.`null`
+         orderView?.typeDoc.should.equal("Sample View")
+         orderView?.viewBodyDefinitions?.size.should.equal(2)
+         orderView?.viewBodyDefinitions?.first()?.bodyType?.qualifiedName?.should?.equal("OrderSent")
+         orderView?.viewBodyDefinitions?.get(1)?.bodyType?.qualifiedName?.should?.equal("OrderSent")
+         orderView?.viewBodyDefinitions?.get(1)?.joinType?.qualifiedName?.should?.equal("OrderFill")
+      }
+
+      it("A join view with two types with multiple join fields can not be compiled") {
+         val src = """
+         type OrderId inherits String
+         model OrderSent {
+            sentOrderId : OrderId
+            tempOrderId : OrderId
+         }
+
+         model OrderFill {
+           fillOrderId: OrderId
+         }
+
+          [[
+           Sample View
+          ]]
+         view OrderView with query {
+            find { OrderSent[] (joinTo OrderFill[]) }
+         }
+           """.trimIndent()
+         val (compilationErrors, _) = Compiler(src).compileWithMessages()
+         compilationErrors.last().detailMessage.should.equal("OrderSent and OrderFill can't be joined")
+      }
+
+      it("multiple Find Statements in a View must be compatible with each other") {
+         val src = """
+         type SentOrderId inherits String
+         type FillOrderId inherits String
+         type OrderEventDateTime inherits Instant
+         type OrderType inherits String
+         type SecurityDescription inherits String
+         type RequestedQuantity inherits String
+         type OrderStatus inherits String
+         type DecimalFieldOrderFilled inherits Decimal
+
+
+         model OrderSent {
+            @Id
+            sentOrderId : SentOrderId
+            @Between
+		      orderDateTime: OrderEventDateTime( @format = "MM/dd/yy HH:mm:ss") by column("Time Submitted")
+            orderType: OrderType by default("Market")
+            subSecurityType: SecurityDescription? by column("Instrument Desc")
+            requestedQuantity: RequestedQuantity? by column("Size")
+            entryType: OrderStatus by default("New")
+         }
+
+         model OrderFill {
+           @Id
+           fillOrderId: FillOrderId
+           orderType: OrderType by default("Market")
+           subSecurityType: SecurityDescription? by column("Instrument Desc")
+           executedQuantity: DecimalFieldOrderFilled? by column("Quantity")
+           entryType: OrderStatus by default("Filled")
+         }
+
+         model OrderEvent { }
+
+          [[
+           Sample View
+          ]]
+         view OrderView inherits OrderEvent with query {
+            find { OrderSent[] } as {
+              orderId: OrderSent.SentOrderId
+              orderDateTime: OrderSent.OrderEventDateTime
+              orderType: OrderSent.OrderType
+              subSecurityType: OrderSent.SecurityDescription
+              requestedQuantity: OrderSent.RequestedQuantity
+              orderEntry: OrderSent.OrderStatus
+            },
+            find { OrderSent[] (joinTo OrderFill[]) } as {
+              orderId: OrderFill.FillOrderId
+              orderDateTime: OrderEventDateTime
+              orderType: OrderFill.OrderType
+              subSecurityType: OrderFill.SecurityDescription
+              requestedQuantity: OrderSent.RequestedQuantity
+              orderEntry: OrderStatus by when {
+                 OrderSent.RequestedQuantity = OrderFill.DecimalFieldOrderFilled -> OrderFill.OrderStatus
+                 else -> "PartiallyFilled"
+              }
+            }
+         }
+           """.trimIndent()
+         val taxiDocument = Compiler(src).compile()
+         val orderFillType = taxiDocument.model("OrderFill")
+         val orderSentType =  taxiDocument.model("OrderSent")
+         val orderEventDateTime = taxiDocument.type("OrderEventDateTime")
+         val orderStatusType = taxiDocument.type("OrderStatus")
+         orderFillType.should.not.be.`null`
+         orderSentType.should.not.be.`null`
+         val orderView = taxiDocument.view("OrderView")
+         orderView.should.not.be.`null`
+         orderView?.typeDoc.should.equal("Sample View")
+         orderView?.viewBodyDefinitions?.size.should.equal(2)
+         val findOrderSent = orderView!!.viewBodyDefinitions!!.first()
+         val joinOrderSentOrderFill = orderView.viewBodyDefinitions!![1]
+         findOrderSent.bodyType.qualifiedName.should.equal("OrderSent")
+         findOrderSent.viewBodyTypeDefinition!!.fields.should.equal(
+            listOf(
+               ViewBodyFieldDefinition(sourceType = orderSentType, fieldType = orderSentType.field("sentOrderId").type, fieldName = "orderId"),
+               ViewBodyFieldDefinition(sourceType = orderSentType, fieldType = orderSentType.field("orderDateTime").type, fieldName = "orderDateTime"),
+               ViewBodyFieldDefinition(sourceType = orderSentType, fieldType = orderSentType.field("orderType").type, fieldName = "orderType"),
+               ViewBodyFieldDefinition(sourceType = orderSentType, fieldType = orderSentType.field("subSecurityType").type, fieldName = "subSecurityType"),
+               ViewBodyFieldDefinition(sourceType = orderSentType, fieldType = orderSentType.field("requestedQuantity").type, fieldName = "requestedQuantity"),
+               ViewBodyFieldDefinition(sourceType = orderSentType, fieldType = orderSentType.field("entryType").type, fieldName = "orderEntry")
+         ))
+         joinOrderSentOrderFill.bodyType.qualifiedName.should.equal("OrderSent")
+         joinOrderSentOrderFill.joinType!!.qualifiedName.should.equal("OrderFill")
+         joinOrderSentOrderFill.viewBodyTypeDefinition!!.fields.should.equal(
+            listOf(
+               ViewBodyFieldDefinition(sourceType = orderFillType, fieldType = orderFillType.field("fillOrderId").type, fieldName = "orderId"),
+               ViewBodyFieldDefinition(sourceType = orderEventDateTime, fieldType = orderEventDateTime, fieldName = "orderDateTime"),
+               ViewBodyFieldDefinition(sourceType = orderFillType, fieldType = orderFillType.field("orderType").type, fieldName = "orderType"),
+               ViewBodyFieldDefinition(sourceType = orderFillType, fieldType = orderFillType.field("subSecurityType").type, fieldName = "subSecurityType"),
+               ViewBodyFieldDefinition(sourceType = orderSentType, fieldType = orderSentType.field("requestedQuantity").type, fieldName = "requestedQuantity"),
+               ViewBodyFieldDefinition(sourceType = orderStatusType, fieldType = orderStatusType, fieldName = "orderEntry")
+            ))
+      }
+
+      it("A join view with two types with single join fields can be compiled") {
+         val src = """
+         type OrderId inherits String
+         model OrderSent {
+            @Id
+            sentOrderId : OrderId
+            tempOrderId : OrderId
+         }
+
+         model OrderFill {
+           @Id
+           fillOrderId: OrderId
+         }
+
+          [[
+           Sample View
+          ]]
+         view OrderView with query {
+            find { OrderSent[] (joinTo OrderFill[]) }
+         }
+           """.trimIndent()
+         val (compilationErrors, _) = Compiler(src).compileWithMessages()
+         compilationErrors.errors().should.be.empty
+      }
+
+      it("find expressions in a view can only contain list types") {
+         val src = """
+         type OrderId inherits String
+         model OrderSent {
+            sentOrderId : OrderId
+            tempOrderId : OrderId
+         }
+
+         model OrderFill {
+           fillOrderId: OrderId
+         }
+
+          [[
+           Sample View
+          ]]
+         view OrderView with query {
+            find { OrderSent (joinTo OrderFill[]) }
+         }
+           """.trimIndent()
+         val (compilationErrors, _) = Compiler(src).compileWithMessages()
+         compilationErrors.last().detailMessage.should.equal("Currently, only list types are supported in view definitions. Replace OrderSent with OrderSent[]")
+      }
+
+      it("find expressions in a view can only contain list types for joins") {
+         val src = """
+         type OrderId inherits String
+         model OrderSent {
+            sentOrderId : OrderId
+            tempOrderId : OrderId
+         }
+
+         model OrderFill {
+           fillOrderId: OrderId
+         }
+
+          [[
+           Sample View
+          ]]
+         view OrderView with query {
+            find { OrderSent[] (joinTo OrderFill) }
+         }
+           """.trimIndent()
+         val (compilationErrors, _) = Compiler(src).compileWithMessages()
+         compilationErrors.last().detailMessage.should.equal("Currently, only list types are supported in view definitions. Replace OrderFill with OrderFill[]")
+      }
+   }
+})
