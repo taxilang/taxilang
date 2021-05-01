39c33c80a048dc2dd641050a7a2d098ff2882b37
Marty Pitt <martypitt@gmail.com>
2021-05-01 09:15:59 +0100

    rename group to org.taxilang, and add language server project. bump version to 1.28.0-SNAPSHOT to align versions from all projects

--- test-utils/src/main/java/lang/taxi/testing/TestHelpers.kt
+++ test-utils/src/main/java/lang/taxi/testing/TestHelpers.kt
@@ -10,55 +10,59 @@ object TestHelpers {
     * Asserts both objects compile to the same taxi object model.
     * Returns the taxi doc from the generated model, for further assertions
     */
-    fun expectToCompileTheSame(generated: List<String>, expected: String):TaxiDocument {
-        return expectToCompileTheSame(generated, listOf(expected))
-    }
+   fun expectToCompileTheSame(generated: List<String>, expected: String): TaxiDocument {
+      return expectToCompileTheSame(generated, listOf(expected))
+   }
+
    /**
     * Asserts both objects compile to the same taxi object model.
     * Returns the taxi doc from the generated model, for further assertions
     */
 
-   fun expectToCompileTheSame(generated: List<String>, expected: List<String>):TaxiDocument {
-        val generatedDoc = Compiler.forStrings(generated).compile()
-        val expectedDoc = Compiler.forStrings(expected).compile()
-        if (generatedDoc == expectedDoc) return generatedDoc
+   fun expectToCompileTheSame(generated: List<String>, expected: List<String>): TaxiDocument {
+      val generatedDoc = Compiler.forStrings(generated).compile()
+      val expectedDoc = Compiler.forStrings(expected).compile()
+      if (generatedDoc == expectedDoc) return generatedDoc
 
-        val typeErros = expectedDoc.types.flatMap { type -> collateTypeErrors(type, generatedDoc) }
-        val serviceErrors = expectedDoc.services.flatMap { service -> collateServiceErrors(service, generatedDoc) }
-        val errors = typeErros + serviceErrors
-        throw AssertionError("Generated docs did not match expected.  Errors: \n" + errors.joinToString("\n"))
-    }
+      val typeErros = expectedDoc.types.flatMap { type -> collateTypeErrors(type, generatedDoc) }
+      val serviceErrors = expectedDoc.services.flatMap { service -> collateServiceErrors(service, generatedDoc) }
+      val errors = typeErros + serviceErrors
+      if (errors.isEmpty()) {
+         return generatedDoc
+      }
+      throw AssertionError("Generated docs did not match expected.  Errors: \n" + errors.joinToString("\n"))
+   }
 
-    private fun collateTypeErrors(type: Type, generatedDoc: TaxiDocument): List<String> {
-        if (!generatedDoc.containsType(type.qualifiedName)) {
-            return listOf("Type ${type.qualifiedName} not present")
-        }
-        val generatedType = generatedDoc.type(type.qualifiedName)
-        val errors = mutableListOf<String>()
-        if (type != generatedType)
-            return listOf("Type ${type.qualifiedName} is not the same as expected")
-        else
-            return emptyList()
-    }
+   private fun collateTypeErrors(type: Type, generatedDoc: TaxiDocument): List<String> {
+      if (!generatedDoc.containsType(type.qualifiedName)) {
+         return listOf("Type ${type.qualifiedName} not present")
+      }
+      val generatedType = generatedDoc.type(type.qualifiedName)
+      val errors = mutableListOf<String>()
+      if (type != generatedType)
+         return listOf("Type ${type.qualifiedName} is not the same as expected")
+      else
+         return emptyList()
+   }
 
-    private fun collateServiceErrors(service:Service, generatedDoc:TaxiDocument):List<String> {
-        if (!generatedDoc.containsService(service.qualifiedName)) {
-            return listOf("Service ${service.qualifiedName} not present")
-        }
-        val generatedService = generatedDoc.service(service.qualifiedName)
-        return service.operations.flatMap { operation ->
+   private fun collateServiceErrors(service: Service, generatedDoc: TaxiDocument): List<String> {
+      if (!generatedDoc.containsService(service.qualifiedName)) {
+         return listOf("Service ${service.qualifiedName} not present")
+      }
+      val generatedService = generatedDoc.service(service.qualifiedName)
+      return service.operations.flatMap { operation ->
 
-            if (!generatedService.containsOperation(operation.name)) {
-                listOf("Operation ${operation.name} is not present")
-            } else {
-                val errors = mutableListOf<String>()
-                val generatedOperation = generatedService.operation(operation.name)
-                if (generatedOperation.contract != operation.contract) errors.add("Contract on ${operation.name} differs from expected")
-                if (generatedOperation.parameters != operation.parameters) errors.add("Parameters on ${operation.name} differs from expected")
-                if (generatedOperation.returnType != operation.returnType) errors.add("Return type on ${operation.returnType} differs from expected")
-                if (generatedOperation.annotations != operation.annotations) errors.add("Annotations on ${operation.name} differs from expected")
-                errors
-            }
-        }
-    }
+         if (!generatedService.containsOperation(operation.name)) {
+            listOf("Operation ${operation.name} is not present")
+         } else {
+            val errors = mutableListOf<String>()
+            val generatedOperation = generatedService.operation(operation.name)
+            if (generatedOperation.contract != operation.contract) errors.add("Contract on ${operation.name} differs from expected")
+            if (generatedOperation.parameters != operation.parameters) errors.add("Parameters on ${operation.name} differs from expected")
+            if (generatedOperation.returnType != operation.returnType) errors.add("Return type on ${operation.returnType} differs from expected")
+            if (generatedOperation.annotations != operation.annotations) errors.add("Annotations on ${operation.name} differs from expected")
+            errors
+         }
+      }
+   }
 }
