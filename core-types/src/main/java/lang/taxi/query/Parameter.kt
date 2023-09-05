c7c053ab416eafc657f665715fdc0f1baa1c9c61
Marty Pitt <martypitt@gmail.com>
2023-09-05 11:42:20 +0100

    feat(taxiql): support query arguments hoisted into scope via given {}
    In an inbound query such as :
    
    ```taxiql
    query RecentOrdersQuery( startDate:Instant, endDate:Instant ) {
       given { startDate, endDate } // <-- this is now supported.
       find { ... }
    ```
    
    This brings better consistency and clarity for how variables are
    exposed into a query scope.

--- core-types/src/main/java/lang/taxi/query/Parameter.kt
+++ core-types/src/main/java/lang/taxi/query/Parameter.kt
@@ -4,6 +4,7 @@ import lang.taxi.accessors.Argument
 import lang.taxi.types.Annotatable
 import lang.taxi.types.Annotation
 import lang.taxi.types.Type
+import lang.taxi.types.TypedValue
 
 data class Parameter(
    override val name: String,
@@ -13,6 +14,31 @@ data class Parameter(
    companion object {
       fun variable(name: String, type: Type, annotations: List<Annotation> = emptyList()): Parameter {
          return Parameter(name, FactValue.Variable(type, name), annotations)
+      }
+   }
+
+   /**
+    * Returns the underlying value.
+    * If this parameter contains a contsant value,
+    * then it is returned.
+    *
+    * Otherwise, attempts to resolve via the named arguments,
+    * matching on name.
+    */
+   fun resolveValue(arguments: Map<String, Any?>): TypedValue {
+      // Arguments are Map<String,Any?> when resolving on an inbound
+      // TaxiQL query.
+      // TODO : Also consider accepting Scopes here? Seems sensible
+      // TODO: Can we run some type checking here too?
+      return if (value is FactValue.Constant) {
+         value.value
+      } else {
+         if (arguments.containsKey(name)) {
+            val argumentValue = arguments[name]
+            TypedValue(value.type, argumentValue)
+         } else {
+            error("Unable to resolve value of parameter $name - no constant is defined, and no matching arguments were provided")
+         }
       }
    }
 
