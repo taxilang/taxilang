f9794b91802c75df1546eea2d847ab2deab28e42
Marty Pitt <martypitt@gmail.com>
2023-09-13 14:53:03 +0100

    feat(stdlib/convert): new convert function added to stdlib

--- core-types/src/main/java/lang/taxi/functions/stdlib/StdLib.kt
+++ core-types/src/main/java/lang/taxi/functions/stdlib/StdLib.kt
@@ -4,22 +4,24 @@ import lang.taxi.functions.vyne.aggregations.Aggregations
 import lang.taxi.types.BuiltIns
 import lang.taxi.types.QualifiedName
 
-object StdLib  {
+object StdLib {
    // Note: because of a bug in the antlr definition,
    // we can't put these in lang.taxi.stdlib. :(
    const val namespace = "taxi.stdlib"
-   val functions = Strings.functions +
-      Aggregations.functions +
-      Functional.functions +
-      Collections.functions +
-      ObjectFunctions.functions +
-      listOf(Coalesce)
+   val functions =
+      Strings.functions +
+         Aggregations.functions +
+         Functional.functions +
+         Collections.functions +
+         ObjectFunctions.functions +
+         Transformations.functions +
+         listOf(Coalesce)
    val taxi = functions.namespacedTaxi() + BuiltIns.taxi
 }
 
-fun stdLibName(name:String):QualifiedName = QualifiedName.from("${StdLib.namespace}.$name")
+fun stdLibName(name: String): QualifiedName = QualifiedName.from("${StdLib.namespace}.$name")
 
-object Coalesce: FunctionApi {
+object Coalesce : FunctionApi {
    override val taxi: String = "declare function coalesce(Any...):Any"
    override val name: QualifiedName = stdLibName("coalesce")
 }
