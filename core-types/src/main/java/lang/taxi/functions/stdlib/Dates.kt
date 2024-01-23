df29c1f312952c07917490f0cb3ce620c9097c2e
Marty Pitt <martypitt@gmail.com>
2024-01-23 15:31:18 +0000

    fix(stdlib/dateMath): relaxed signatures for addDays | addMinutes etc., until we have overload support

--- core-types/src/main/java/lang/taxi/functions/stdlib/Dates.kt
+++ core-types/src/main/java/lang/taxi/functions/stdlib/Dates.kt
@@ -13,22 +13,35 @@ object Dates {
 }
 
 object AddMinutes : FunctionApi {
-   override val taxi: String = """declare function addMinutes(DateTime, Int):DateTime
-      |declare function addMinutes(Instant, Int): Instant
-   """.trimMargin()
+   // TODO  : Overload support needed
+   // We used to declare this as:
+   // declare function addMinutes(DateTime, Int):DateTime
+   // declare function addMinutes(Instant, Int): Instant
+   // However, we don't support overloads, so this fails at evaluation time.
+   // (See DateMathTests)
+   override val taxi: String = """declare function <T> addMinutes(T, Int):T"""
    override val name: QualifiedName = stdLibName("dates.addMinutes")
 }
 
 object AddSeconds : FunctionApi {
-   override val taxi: String = """declare function addSeconds(DateTime, Int):DateTime
-      |declare function addSeconds(Instant, Int): Instant
-   """.trimMargin()
+   // TODO  : Overload support needed
+   // We used to declare this as:
+   // declare function addSeconds(DateTime, Int):DateTime
+   // declare function addSeconds(Instant, Int): Instant
+   // However, we don't support overloads, so this fails at evaluation time.
+   // (See DateMathTests)
+   override val taxi: String = """declare function <T> addSeconds(T, Int):T"""
    override val name: QualifiedName = stdLibName("dates.addSeconds")
 }
 
 object AddDays : FunctionApi {
-   override val taxi: String = """declare function addDays(DateTime, Int):DateTime
-      |declare function addDays(Instant, Int): Instant
+   // TODO  : Overload support needed
+   // We used to declare this as:
+   // declare function addDays(DateTime, Int):DateTime
+   // declare function addDays(Instant, Int): Instant
+   // However, we don't support overloads, so this fails at evaluation time.
+   // (See DateMathTests)
+   override val taxi: String = """declare function <T> addDays(T, Int):T
    """.trimMargin()
    override val name: QualifiedName = stdLibName("dates.addDays")
 }
