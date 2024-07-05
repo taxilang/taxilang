2e1f5b251d67044405103e76acb3d5f388b2ec02
Marty Pitt <martypitt@gmail.com>
2024-07-05 05:50:32 +0100

    feat(avro): added annotation for AvroMessageWrapper, allowing indicating that a message type contains an avro message, and should be decoded as a bytearray

--- avro-to-taxi/src/main/java/lang/taxi/generators/avro/AvroMessageAnnotation.kt
+++ avro-to-taxi/src/main/java/lang/taxi/generators/avro/AvroMessageAnnotation.kt
@@ -8,9 +8,22 @@ object AvroAnnotationSchema {
 }
 object AvroMessageAnnotation {
    val NAME = "lang.taxi.formats.AvroMessage"
+   val AVRO_MESSAGE_WRAPPER_NAME = "lang.taxi.formats.AvroMessageWrapper"
    val taxi = """
       namespace lang.taxi.formats {
          annotation ${NAME.toQualifiedName().typeName}
+
+         [[
+         Indicates that a model is a wrapper around an AvroMessage.
+         This should be used when embedding an Avro message within another message, such as to capture
+         Kafka message keys or headers.
+
+        The message payload will be treated as an Avro message, even if an Avro schema is not explicitly declared for this type.
+
+         Any fields declared within this message, aside from the Avro message itself, must be populated
+         through expressions or meta-value suppliers (e.g., KafkaMessageKey or KafkaMessageHeader).
+         ]]
+         annotation AvroMessageWrapper
       }
    """.trimIndent()
 
