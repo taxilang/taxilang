ef87f2214b57e4517c6357d95eb66105201ca770
Serhat Tuncay <serhat.tuncay@vyne.co>
2024-08-29 16:03:27 +0300

    Fixed typo in Http schema

--- taxi-stdlib-annotations/src/main/java/lang/taxi/annotations/Http.kt
+++ taxi-stdlib-annotations/src/main/java/lang/taxi/annotations/Http.kt
@@ -56,6 +56,7 @@ data class HttpService(val baseUrl: String) : AnnotationProvider {
             annotation HttpQueryVariable { value: String }
             
             annotation HttpResponseHeader { value: String }
+         }
            
             
       """
