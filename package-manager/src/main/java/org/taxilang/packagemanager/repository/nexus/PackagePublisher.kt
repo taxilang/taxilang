6da232f9977e70edbe7bab081a1f71179c0ec9a5
Marty Pitt <martypitt@gmail.com>
2024-12-02 14:48:23 +0000

    feat(cli): added package command, which creates a standalone bundle of a taxi project
    ORB-861

--- package-manager/src/main/java/org/taxilang/packagemanager/repository/nexus/PackagePublisher.kt
+++ package-manager/src/main/java/org/taxilang/packagemanager/repository/nexus/PackagePublisher.kt
@@ -2,9 +2,11 @@ package org.taxilang.packagemanager.repository.nexus
 
 import lang.taxi.packages.Credentials
 import lang.taxi.packages.ReleaseType
+import lang.taxi.packages.TaxiPackageProject
 import lang.taxi.packages.TaxiProjectLoader
 import lang.taxi.utils.log
 import org.taxilang.packagemanager.TaxiPackageBundler
+import org.taxilang.packagemanager.TaxiProjectBundle
 import java.nio.file.Files
 import java.nio.file.Path
 
@@ -12,17 +14,23 @@ class PackagePublisher(
    private val serviceFactory: PackageServiceFactory = DefaultPackageServiceFactory,
    private val credentials: List<Credentials> = emptyList()
 ) {
-   // TODO: Can we remove releaseType?
-   fun publish(projectBasePath: Path, releaseType: ReleaseType? = null) {
+
+   fun createBundle(projectBasePath: Path): Pair<TaxiPackageProject,TaxiProjectBundle> {
       val packageFile = projectBasePath.resolve("taxi.conf")
       require(Files.exists(packageFile)) { "Project file $packageFile doesn't exist" }
 
       val project =
          TaxiProjectLoader(packageFile).load()
-      val publishToRepository = project.publishToRepository
-         ?: error("Cannot publish without a publishToRepository defined")
 
       val bundle = TaxiPackageBundler.createBundle(projectBasePath, project.identifier)
+      return project to bundle
+   }
+
+   // TODO: Can we remove releaseType?
+   fun publish(projectBasePath: Path, releaseType: ReleaseType? = null) {
+      val (project,bundle) = createBundle(projectBasePath)
+      val publishToRepository = project.publishToRepository
+         ?: error("Cannot publish without a publishToRepository defined")
 
       log().info("Publishing package ${project.identifier.id} from ${bundle.zip.toAbsolutePath()} to ${publishToRepository.url}")
 
