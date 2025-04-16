561576a76f02ed7b0f65dfc9fb2ccd8dd74bf7a7
Marty Pitt <martypitt@gmail.com>
2025-04-16 17:49:25 +0000

    Revert "Merge branch 'array-access' into 'develop'"
    This reverts merge request !585

--- core-types/src/main/java/lang/taxi/expressions/Expression.kt
+++ core-types/src/main/java/lang/taxi/expressions/Expression.kt
@@ -301,10 +301,3 @@ fun List<Expression>.toExpressionGroup(): Expression {
       ExpressionGroup(this)
    }
 }
-
-
-data class ArrayAccessExpression(val arrayInstanceExpression: Expression, val arrayIndexExpression: Expression,
-                                 override val compilationUnits: List<CompilationUnit>) : Expression() {
-   override val returnType: Type
-      get() = Arrays.unwrapPossibleArrayType(arrayInstanceExpression.returnType)
-}
