f7cd9542aebacdee323ce1fc039c3753d1576c6a
Marty Pitt <martypitt@gmail.com>
2025-08-04 18:02:04 +0100

    fix(docs): fix incorrect example in docs

--- core-types/src/main/java/lang/taxi/functions/stdlib/Collections.kt
+++ core-types/src/main/java/lang/taxi/functions/stdlib/Collections.kt
@@ -678,9 +678,9 @@ object Filter : FunctionApi, HasRunnableExamples {
                }
                find {
                   // Names of seniors (over 70)
-                  seniorFriends: Friend[] = friends.filter((friend:Friend) -> friend.age > 70) as Name[]
+                  seniorFriends: Name[] = friends.filter((friend:Friend) -> friend.age > 70) as Name[]
                   // Names of young adults (under 30)
-                  youngFriends: Friend[] = friends.filter((friend:Friend) -> friend.age < 30) as Name[]
+                  youngFriends: Name[] = friends.filter((friend:Friend) -> friend.age < 30) as Name[]
                }
             """.trimIndent(),
             expectedJson = """{
